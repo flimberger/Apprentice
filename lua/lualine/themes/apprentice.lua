22cf24c1ad4feb8beb9bafc45e0491f367ec948b
Florian Limberger <florian.limberger@inovex.de>
2023-04-20 18:01:56 +0200

    Add lualine theme
    Add a lua module for the Apprentice palette and add a lualine theme.

---
+++ lua/lualine/themes/apprentice.lua
@@ -0,0 +1,28 @@
+local p = require('apprentice/palette')
+return {
+	normal = {
+		a = { bg = p.light_blue, fg = p.darker_grey },
+		b = { bg = p.dark_grey, fg = p.light_blue },
+		c = { bg = p.ocre, fg = p.darker_grey },
+	},
+	insert = {
+		a = { bg = p.light_green, fg = p.darker_grey },
+		b = { bg = p.dark_grey, fg = p.light_green },
+	},
+	visual = {
+		a = { bg = p.green, fg = p.darker_grey },
+		b = { bg = p.dark_grey, fg = p.green },
+	},
+	replace = {
+		a = { bg = p.purple, fg = p.darker_grey },
+		b = { bg = p.dark_grey, fg = p.purple },
+	},
+	command = {
+		a = { bg = p.blue, fg = p.darker_grey },
+		b = { bg = p.dark_grey, fg = p.blue },
+	},
+	inactive = {
+		a = { bg = p.medium_grey, fg = p.darker_grey },
+		b = { bg = p.dark_grey, fg = p.medium_grey },
+	},
+}
